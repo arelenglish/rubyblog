768dc78ddbc2af0cfd03282a1325a20c648a7f71
Arel English <arelenglish@gmail.com>
2012-05-01 20:23:50 -0400

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120501221211) do
+ActiveRecord::Schema.define(:version => 20120502001222) do
 
   create_table "article_tags", :force => true do |t|
     t.integer  "article_id"
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(:version => 20120501221211) do
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "author_id"
+    t.integer  "category_id"
   end
 
   create_table "authors", :force => true do |t|
