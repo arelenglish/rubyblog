fad7d15aaa956f114869965a7178dc23642f58a0
Arel English <arelenglish@gmail.com>
2012-05-01 20:23:50 -0400

    first commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ActiveRecord::Base
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :author_id, :category_id
   belongs_to :author
   belongs_to :category
   has_many :article_tags
   has_many :tags, :through => :article_tags
+  
+  
 end
