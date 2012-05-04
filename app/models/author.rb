c5dfdcb2451111b45b553dc1b0679f1729fae4ff
Arel English <arelenglish@gmail.com>
2012-05-04 19:50:20 -0400

    lots of crazy aditionsgit add .!

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,12 @@
 class Author < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :twitter_handle
   has_many :articles
+  
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+  
+  def formatted_created_at
+    self.created_at.strftime('%b %d %I:%M%p') if self.created_at
+  end
 end
