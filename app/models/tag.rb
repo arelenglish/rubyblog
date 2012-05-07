5b55deb1a6d8f96c06a81e8282c5ac01e196382d
Arel English <arelenglish@gmail.com>
2012-05-07 17:59:11 -0400

    More minor updates

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ActiveRecord::Base
   attr_accessible :name
   has_many :article_tags
   has_many :articles, :through => :article_tags
+  
+  def formatted_created_at
+    self.created_at.strftime('%b %d') if self.created_at
+  end
 end
