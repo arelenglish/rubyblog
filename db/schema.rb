67e8e18f27feb2d72f72767e394e776ce1e2be1e
Arel English <arelenglish@gmail.com>
2012-05-15 23:59:09 -0400

    added authentication and authorization. Still need to add authorization to other places

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120512031145) do
+ActiveRecord::Schema.define(:version => 20120516022156) do
 
   create_table "article_tags", :force => true do |t|
     t.integer  "article_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20120512031145) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "email"
+    t.string   "role"
   end
 
 end
