1062370b1ac9bbae814f23e6cfd38347c3906f54
Arel English <arelenglish@gmail.com>
2012-05-17 23:56:27 -0400

    trying to mass assign roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
   has_many :articles
   
-  attr_accessible :username, :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation :role
   has_secure_password
   validates_presence_of :password, :email, :on => :create
   validates :email, :uniqueness => true
   
   ROLES = %W[admin moderator]
   
+  def formatted_created_at
+    self.created_at.strftime('%b %d') if self.created_at
+  end
+ 
 end
