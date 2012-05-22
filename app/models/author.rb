ec1ed916dd78a4f5c1d18b638a7e4ae01941dd98
Arel English <arelenglish@gmail.com>
2012-05-22 15:47:48 -0400

    Finally got tagging working

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,6 @@
 class Author < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :twitter_handle
-  has_many :articles
-  has_many :rolls
-  
+  has_many :articles  
   
   def full_name
     "#{self.first_name} #{self.last_name}"
