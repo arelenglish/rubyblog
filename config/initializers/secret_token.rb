c46125fae3e744795ff5304b6a2ffef7fb2f0d33
Arel English <arelenglish@gmail.com>
2012-06-02 15:36:07 -0400

    canged secret key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rubyblog::Application.config.secret_token = 'af87652a40ffca9d64420569138fe4aa41f0e6c316705f4836cb474c8cdb701ac155c694e5294178b70a6e77d58e3740f89ef5b6efe87e23ae3d11b7e557932c'
+Rubyblog::Application.config.secret_token = '79c8d7e6d3f3060271d415c2203a95507e0aef38e62fb0a9f2a1e68b3c50be3610933062148f2161e12b2962e5b1a04e2bbbe3109af5e7e0bbd0f1020cf6ddaf'
