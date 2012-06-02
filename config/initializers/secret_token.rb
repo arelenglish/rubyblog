8d06e943b5b7bdf70dd4d53a0671408a44f05d55
Arel English <arelenglish@gmail.com>
2012-06-02 15:48:46 -0400

    changed path for secret_token

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rubyblog::Application.config.secret_token = 'fda53ba7d893a4b7a243762a7072df364aa44921f1768d63981ddf477b4c4218b208b258b1ebcd81936ff831215689e2d2ccb9a8e152469cebcd40fff8ed82c7'
