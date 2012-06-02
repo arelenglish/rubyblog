6e0a11d4506b692d03ed336a88cb873e594e6c9f
Arel English <arelenglish@gmail.com>
2012-06-02 15:37:10 -0400

    asdfj

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rubyblog::Application.config.secret_token = '79c8d7e6d3f3060271d415c2203a95507e0aef38e62fb0a9f2a1e68b3c50be3610933062148f2161e12b2962e5b1a04e2bbbe3109af5e7e0bbd0f1020cf6ddaf'
+Rubyblog::Application.config.secret_token = 'asdf79c8d7e6d3f3060271d415c2203a95507e0aef38e62fb0a9f2a1e68b3c50be3610933062148f2161e12b2962e5b1a04e2bbbe3109af5e7e0bbd0f1020cf6ddaf'
