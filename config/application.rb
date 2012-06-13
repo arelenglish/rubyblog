56ecbd2efa8a59e58435e89c3ac32fc26cf9f4ea
Arel English <arelenglish@gmail.com>
2012-06-12 20:36:21 -0400

    trying to get heroku to work

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,9 @@ module Rubyblog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Trying to get Heroku to work with my app
+    config.assets.initialize_on_precompile = false
+    
   end
 end
